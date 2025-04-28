ada29dd036064ad73b9b39061f5ce919e66622a1
PratikPatil <pratikpatil57432@gmail.com>
2025-04-28 14:06:54 +0530

    Profile created

---
+++ script/script.js
@@ -0,0 +1,8 @@
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const navLinks = document.getElementById('nav-links');
+
+
+hamburgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active'); 
+});
